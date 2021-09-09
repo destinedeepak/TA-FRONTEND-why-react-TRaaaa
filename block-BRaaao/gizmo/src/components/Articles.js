d39d224a361d5e779928f9f721944876eb08826c
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-09-09 15:59:30 +0530

    Assignment: Articles App, Completed

---
+++ block-BRaaao/gizmo/src/components/Articles.js
@@ -0,0 +1,37 @@
+import data from '../data';
+function Articles(){
+    return (
+         <section className="articles container">
+             <h2 className="center">
+                 Articles
+             </h2>
+             <div className="flex wrap">
+                 {data.map((article) =>(
+                    <Article {...article} />
+                 ))}
+             </div>
+         </section>
+    )
+}
+
+function Article(props){
+    function getDate(date){
+        return new Date(date);
+    }
+    return (
+        <>
+        <article className="flex-28">
+                     <img src={props.urlToImage} alt="" />
+                     <div className="text">
+                         <h4 >{props.title}</h4>
+                         <h5 >by: {props.author}</h5>
+                         <h5 >published on {getDate(props.publishedAt).getDate()}/{getDate(props.publishedAt).getMonth()}/{getDate(props.publishedAt).getFullYear()}</h5>
+                         <p>{props.content.slice(0,50)}...</p>
+                         <a href={props.url} target="_blank" className="center btn">Learn More</a>
+                     </div>
+                 </article>
+        </>
+    )
+}
+
+export default Articles;
---
+++ block-BRaaao/gizmo/src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer (){
+    return (
+        <>
+        <hr></hr>
+        <h2>2020 All rights reserved</h2>
+        </>
+    )
+}
+
+export default Footer;
